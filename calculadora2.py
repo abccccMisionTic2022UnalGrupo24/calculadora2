10c28c84835c164a8b8ad2f7d1e20bfca5c2e3eb
Camartinezm92 <Camartinezm92@gmail.com>
2022-08-25 22:57:21 -0500

    Update calculadora2.py

--- calculadora2.py
+++ calculadora2.py
@@ -1,4 +1,24 @@
 opcion=False
+sencilla = 20000
+doble = 25000
+triple = 28000
+total = 0
+l_prod = []
+l_total = []
+l_cantidad = []
+opcion = False
+
+def add(c):
+        op = input("Desea ordenar algo mas? oprima S o N").upper()
+        if (op == "S"):
+            cantidad = int (input("Cantidad de Hamburguesas"))
+            tot = c * cantidad  
+            l_prod.append(texto)
+            l_total.append(tot)
+            l_cantidad.append(cantidad)  
+
+
+
 
 print("Bienvenidos al restaurante hamburguers abcccc \nPor favor eliga una de las siguientes opciones para pedir una hamburguesa: ")
 print("1. Hamburguesa sencilla... $20.000 pesos")
@@ -7,14 +27,30 @@ print("3. Hamburguesa triple... $28.000 pesos" )
 
 while opcion==False:
     opcion_hamburguesa=input("CUAL DESEAS ORDENAR: ")
+    opcion_hamburguesa=input("CUAL DESEAS ORDENAR: ").upper()
     print("La opcion elegida fue: ", opcion_hamburguesa)
 
     if(opcion_hamburguesa == "1"):
         print("La hamburguesa que usted pidió fue: Hamburguesa sencilla, con un valor de $20.000 pesos")
+        texto = "Hamburguesa Sencilla"
+        c = sencilla
+        add(c)
     elif(opcion_hamburguesa == "2"):
         print("La hamburguesa que usted pidió fue: Hamburguesa doble, con un valor de $25.000 pesos")
+        texto = "Hamburguesa Doble"
+        c = doble
+        add(c)
     elif(opcion_hamburguesa == "3"):
         print("La hamburguesa que usted pidió fue: Hamburguesa triple, con un valor de $28.000 pesos")
+        texto = "Hamburguesa Triple"
+        c = triple
+        add(c)
+    elif(opcion_hamburguesa == "SALIR"):
+        opcion = True
+        for i in range(len(l_prod)):
+            print(l_cantidad[i], l_prod[i], l_total[i])
+
+        print("El valor total es: ", sum(l_total))    
     else:
         print("Por favor eligir una opción valida\n")
 
@@ -36,4 +72,5 @@ if(medio_de_pago == 1):
 elif(medio_de_pago == 2):
     print("El medio de pago con el cual usted va a pagar es: Otro medio de pago, con un recargo del 0%")
 else:
+    print("Por favor eligir una opción valida\n")
     print("Por favor eligir una opción valida\n")
