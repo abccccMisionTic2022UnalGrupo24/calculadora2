66a34e7b132acb257774150781d4ea60f294e722
Camartinezm92 <Camartinezm92@gmail.com>
2022-08-27 09:48:50 -0500

    por perro!!!

--- calculadora2.py
+++ calculadora2.py
@@ -1,7 +1,7 @@
 sencilla = 20000
 doble = 25000
 triple = 28000
-total = 0
+total=0
 l_prod = []
 l_total = []
 l_cantidad = []
@@ -18,12 +18,11 @@ d=0
 e=True
 beb=[]
 canbeb=[]
+sw=True
+
 
 
 def ivad(bebidadulce,cantidad):
-    text="Gaseosas o Jugos "
-    beb.append(text)
-    canbeb.append(cantidad)
     gascan = bebidadulce * cantidad
     totalconiva= (gascan*1.19)
     return totalconiva
@@ -44,47 +43,22 @@ def siniva(agua,cantidad):
     totalsinivac= (agu*1.19)
     return totalsinivac
 
-"""while e==True:
-    Entradadetexto=input("Ingrese el tipo de bebidas que desea: \n 1 Gaseosa o Jugo \n 2. Agua  \n 3. Cerveza \n C. Continuar \n Ingrese el tipo segun se espeficica y la cantidad: ")
-    Entradadetexto=Entradadetexto.lower()
-
-
-
-    if (Entradadetexto == "1" or Entradadetexto=="gaseosa"):
-        cantidad=int(input("Ingrese la contidad de bebidas que desea: "))
-        r= ivad(bebidadulce,cantidad)
-    elif (Entradadetexto == "3" or Entradadetexto=="cerveza"):
-        cantidad=int(input("Ingrese la contidad de bebidas que desea: "))
-        c= ivac(cerveza,cantidad)
-    elif (Entradadetexto == "2" or Entradadetexto=="agua"):
-        cantidad=int(input("Ingrese la contidad de bebidas que desea: "))
-        f=siniva(agua,cantidad)
-    else:
-        print(" Bebidas confirmadas!!!!")      
-        e=False
-"""
 def Eliminar():
     for i in range(len(l_prod)):
         print(i+1,l_cantidad[i], l_prod[i], l_total[i])
     l=int(input("Indique el producto que desea eliminar:"))
     l=l-1
-    #if (delete == "hamburguesa sensilla" or delete == "sensilla" or delete == "1"):
-     #   remo="Hamburguesa Sencilla"
-      #  for i in range((len(l_prod))):
-       #     remo2=l_prod[(i-1)]
-         #   print(remo)
-          #  if (remo == remo2):
-
     del l_prod[l]
     del l_cantidad[l]
     del l_total[l]
     for i in range(len(l_prod)):
         print(i+1,l_cantidad[i], l_prod[i], l_total[i])
-    #    caso = True        
+        caso = True        
         
     
 def add(texto,c):
-    cantidad = int (input("Cantidad de Hamburguesas: "))
+    print("Cantidad de "+ texto)
+    cantidad = int(input("que desea ordenar: "))
     tot = c * cantidad  
     cont=+1
     l_prod.append(texto)
@@ -94,18 +68,23 @@ def add(texto,c):
     op = input("Desea ordenar algo mas? oprima S o N: ").upper()
     if (op == "S"):
         caso = False
-        Menu(caso)
     else:
         caso = True
-        print("Lo que usd ordeno fue:\n ------------------------------")
+        print("\n ---EL PEDIDO REALIZADO ES---\n ------------------------------ \n ------------------------------")
         for i in range(len(l_prod)):
             print(i+1,l_cantidad[i], l_prod[i], l_total[i])
-        print("------------------------------ \n El valor total es: ", sum(l_total)) 
-        check= (input(print(" ------------------------------ \n desea eliminar algun item S/N:  "))).upper()
+        total=int(sum(l_total))
+        print("------------------------------ \n El valor total es:       ", total ) 
+        check= input(" ------------------------------ \n ------------------------------ \n desea eliminar algun item S/N: ").upper()
         if (check == "S"):
             Eliminar()
-            caso=True
-          
+        else:    
+            caso = True
+        
+            
+def again(total,l_prod,l_total,l_cantidad,contar,caso,cantidad,c,d,e,r,beb,canbeb):
+    Menu(caso)
+    mediopago(total,l_cantidad,l_prod,l_total)
                 
 def Menu(caso):
     print("1. Hamburguesa sencilla  ... $20.000 pesos")
@@ -116,72 +95,115 @@ def Menu(caso):
     print("6. Agua                  ... $ 4.000 pesos" )
     print("7. Eliminar Item de la orden")
     print("8. Salir para continuar con el medio de pago")
-    opcion=caso
-    while opcion==False:
-        opcion_hamburguesa=input("CUAL DESEAS ORDENAR: ").upper()
+    while caso==False:
+        opcion_hamburguesa=input("-------\nQUE DESEA: ").upper()
         print("La opcion elegida fue: ", opcion_hamburguesa)
 
         if(opcion_hamburguesa == "1"):
             print("La hamburguesa que usted pidió fue: Hamburguesa sencilla, con un valor de $20.000 pesos")
-            texto = "Hamburguesa Sencilla"
+            texto = "Hamburguesa Sencilla  "
             c = sencilla
             add(texto,c)
         elif(opcion_hamburguesa == "2"):
             print("La hamburguesa que usted pidió fue: Hamburguesa doble, con un valor de $25.000 pesos")
-            texto = "Hamburguesa_Doble"
+            texto = "Hamburguesa_Doble     "
             c = doble
             add(texto,c)
         elif(opcion_hamburguesa == "3"):
             print("La hamburguesa que usted pidió fue: Hamburguesa triple, con un valor de $28.000 pesos")
-            texto = "Hamburguesa Triple"
+            texto = "Hamburguesa Triple    "
             c = triple
             add(texto,c)
         elif(opcion_hamburguesa == "4"):
             print("Ha seleccionado Gaseosas o Jugos, con un valor de $3.000 pesos")
-            texto = "Gaseosa o Jugo"
+            texto = "Gaseosas o Jugos      "
             c = bebidadulce
             add(texto,c)
         elif(opcion_hamburguesa == "5"):
             print("Ha seleccionado cerveza, con un valor de $4.000 pesos")
-            texto = "Cerveza"
+            texto = "Cervezas              "
             c = cerveza
             add(texto,c)
         elif(opcion_hamburguesa == "6"):
             print("Ha seleccionado Agua, con un valor de $4.000 pesos")
-            texto = "Agua"
+            texto = "Aguas                 "
             c = agua
             add(texto,c)
         elif(opcion_hamburguesa == "7"):
             Eliminar()
-            caso-True
-        elif(opcion_hamburguesa == "SALIR"):
-            opcion = True
-            for i in range(len(l_prod)):
-                print(l_cantidad[i], l_prod[i], l_total[i])
-            print("El valor total es: ", sum(l_total))    
+           # caso=True
+        elif(opcion_hamburguesa == "SALIR" or opcion_hamburguesa == "S"):
+            caso = True    
         else:
             print("Por favor eligir una opción valida\n")
 
-def mediopago():  
+def mediopago(total,l_cantidad,l_prod,l_total):  
+
+    print("Por favor eligir una opción valida\n")
     print("\nPor favor eliga el medio de pago con el cual usted va a cancelar su pedido por medio de las siguientes opciones: ")
-    print("1. Tarjeta de crédito (Se realiza un recargo del 7½)")
+    print("1. Tarjeta de crédito (Se realiza un recargo del 7%)")
     print("2. Otros medios de pago (no se realiza ningún tipo de recargo 0%)")
-    medio_de_pago=int(input())
-
-    while(medio_de_pago > 2):
-        print("Por favor eligir una opción valida\n")
-        print("\nPor favor eliga el medio de pago con el cual usted va a cancelar su pedido por medio de las siguientes opciones: ")
-        print("1. Tarjeta de crédito (Se realiza un recargo del 7½)")
-        print("2. Otros medios de pago (no se realiza ningún tipo de recargo 0%)")
-        medio_de_pago=int(input())
-
-        if(medio_de_pago == 1):
-            print("El medio de pago con el cual usted va a pagar es: Tarjeta de crédito, con un recargo del 7%")
-        elif(medio_de_pago == 2):
-            print("El medio de pago con el cual usted va a pagar es: Otro medio de pago, con un recargo del 0%")
+    medio_de_pago=input("Indique una opcion: ")
+   
+    
+    if(medio_de_pago == "1"):
+        print("El medio de pago con el cual usted va a pagar es: Tarjeta de crédito, con un recargo del 7%")
+        total=sum(l_total)
+        Valor_Recargo=((total*7)/100)
+        print("El recargo por pago con tarjeta es de: ", Valor_Recargo )
+        des=input("Continuar con el proceso de pago de tarjeta? S/N: ").upper()
+        if (des=="S"):
+            print("\n ---EL PEDIDO REALIZADO ES---\n ------------------------------ \n ------------------------------")
+            for i in range(len(l_prod)):
+                print(i+1,l_cantidad[i], l_prod[i], l_total[i])
+            print("------------------------------ \n ------------------------------")
+            print("El total sin recargo:     ",total)
+            print("Recargo:                  ",Valor_Recargo)
+            print("------------------------------")
+            print("total a pagar             ",total+Valor_Recargo)
+            acep=input("Desea impimir el recibo? S/N").upper()
+            if (acep=="S"):
+                print("Imprimiendo.......")
+        
         else:
-            print("por favor eligir una opción valida\n") 
+            mediopago(total,l_cantidad,l_prod,l_total)
+            
+    elif(medio_de_pago == "2"):
+        total=sum(l_total)
+        print("El medio de pago con el cual usted va a pagar es: Otro medio de pago, con un recargo del 0%")
+        print("\n ---EL PEDIDO REALIZADO ES---\n ------------------------------ \n ------------------------------")
+        for i in range(len(l_prod)):
+                print(i+1,l_cantidad[i], l_prod[i], l_total[i])
+        print("------------------------------")
+        print("El total a pagar es       ",total)
+        acep=input("Desea impimir el recibo? S/N: ").upper()
+        if (acep=="S"):
+            print("Imprimiendo.......")
+
+    else:
+        print("por favor eligir una opción valida\n") 
+        mediopago(total,l_cantidad,l_prod,l_total)
+
 
 print("Bienvenidos al restaurante hamburguers abcccc \nPor favor eliga una de las siguientes opciones para pedir una hamburguesa: ")
-Menu(caso)
-mediopago()
+while sw==True:
+    conti=input("Seleccione una opcion:\n 1. Nuevo cliente\n 2. Cerrar proceso \n Ingrse la opcion: ")
+    if (conti=="1"):
+        total=0
+        l_prod = []
+        l_total = []
+        l_cantidad = []
+        contar=[]
+        caso=False
+        cont=0
+        cantidad=0
+        c=0
+        r=0
+        d=0
+        e=True
+        beb=[]
+        canbeb=[]
+        again(total,l_prod,l_total,l_cantidad,contar,caso,cantidad,c,d,e,r,beb,canbeb)
+    elif(conti=="2"):
+        sw=False
+print("Hasta luego....")
